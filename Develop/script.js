03e59057ac30ce69f0ee054c4d4b553055c5654a
GSvenny <gabrielswagner7@gmail.com>
2022-05-13 20:08:33 -0500

    added password criteria prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,11 @@
 // Assignment code here
 
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,6 +14,18 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+  passwordLength = alert("Welcome to the Password Generator.");
+                   alert("For your password, you are going to need to supply:")
+                   alert("A password length (between 8 to 128 characters")
+                   alert("at least (1) lowercase, and (1) uppercase character")
+                   alert("at least (1) number")
+                   alert("and finally at least one special character")
 }
 
 // Add event listener to generate button
