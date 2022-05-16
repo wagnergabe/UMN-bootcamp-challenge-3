00886657a051284aaeef7eb80a362b730ac024a5
GSvenny <gabrielswagner7@gmail.com>
2022-05-15 19:25:54 -0500

    fixed input fails

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,19 @@
 // Assignment code here
 //**WHEN prompted for the length of the password**//
 //**THEN I choose a length of at least 8 characters and no more than 128 characters**//
-function passLength () {
-var passwordLength = parseIntprompt("please choose between 8 and 128 characters");
-  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null) {
+var choiceArr = [] //Added this array sign, think that fixed the error for the .flat funtion
+var number= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var lowerCase = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "F", "G", ",H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"]
+//source: https://owasp.org/www-community/password-special-characters
+
+
+
+
+function getPrompts() {
+var characterLength = parseIntprompt("please choose a length between 8 and 128 characters");
+  if (characterLength < 8 || characterLength > 128 || characterLength === null || isNan(characterLength)) {
     alert("please choose a number between 8 and 128");
     passLength();
   }
@@ -12,32 +22,38 @@ var passwordLength = parseIntprompt("please choose between 8 and 128 characters"
 
 //WHEN asked for character types to include in the password
 //THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-var chooseExtras;
-var number= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lowerCase = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "F", "G", ",H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"]
-//source: https://owasp.org/www-community/password-special-characters
 
+function extraCharacters() {
 if (confirm("Would You like any numbers in your password?")) {
-chooseExtras = true;
+chooseExtras.push(number);
 alert("You have chosen to include a number");
 }
 else {alert("no numbers in this password");
-}
+};
 
-if (confirm("how about a lower case letter?"))
-chooseExtras = lowerCase.push;
+if (confirm("how about a lower case letter?")) {
+chooseExtras.push(lowerCase); 
 alert("you have chosen a lowercase letter")
+}else {alert("You've skipped on the lower case option")
+};
 
-console.log(chooseExtras)
+if (confirm("Fancy an uppercase letter?")) {
+chooseExtras.push(upperCase);
+alert ("You GOT an UPPERCASE in your password")
+} else {alert ("No capital letters allowed in this password")
+};
+
+if (confirm("Garnish with a $pecial Character?")) {
+chooseExtras = chooseExtras.push(specialCharacters);
+alert("Your password now has extra special in it") 
+} else {alert("You're special enough!")
+};
+chooseExtras.flat()
+console.log(chooseExtras);
+} 
+//.push/.flat source = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
 
   
-
-
-
-
-
 
 
 
@@ -54,20 +70,15 @@ function writePassword() {
 
 }
 
-function generatePassword() {
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-  var passwordCriteria = alert("Welcome to the Password Generator.");
-                   alert("For your password, you are going to need to supply:")
-                   alert("A password length (between 8 to 128 characters)")
-                   alert("at least (1) lowercase, and (1) uppercase character")
-                   alert("at least (1) number")
-                   alert("and finally at least one special character")
-passLength();
-smallLetters();
-  
-}
+
+
+
+                 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword{
+
+}
